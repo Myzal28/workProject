6892d6817080d6537767173e741ad44c5fe4b4c4
Maxime Lalo <maxime.lalo.pro@gmail.com>
2019-03-05 17:03:10 +0100

    Save 05/03

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,14 +4,22 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+/**
+ * Class AdminController
+ * @package App\Controller
+ * @Route("/admin")
+ * @IsGranted("ROLE_ADMIN")
+ */
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("", name="admin_index")
      */
     public function index()
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN',null,"Vous n'êtes pas admin");
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
